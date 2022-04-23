f8dffb493edc5de11ce422d541a64cbeb67de26b
wwren <rdding@student.unimelb.edu.au>
2022-04-23 12:45:12 +1000

    solution

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { data } from "./data.js";
+import { ReactComponent as PatternBg } from "./img/pattern-bg.svg";
+import { ReactComponent as PatternCurve } from "./img/pattern-curve.svg";
+import { ReactComponent as PatternQuotes } from "./img/pattern-quotes.svg";
+import { ReactComponent as Prev } from "./img/icon-prev.svg";
+import { ReactComponent as Next } from "./img/icon-next.svg";
+import { useState } from "react";
 function App() {
+  const [currIdx, setCurrIdx] = useState(0);
+  const clickPrev = () => {
+    if (currIdx != 0) {
+      setCurrIdx(currIdx - 1);
+    }
+  };
+
+  const clickNext = () => {
+    if (currIdx != data.length - 1) {
+      setCurrIdx(currIdx + 1);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="quote">
+          <div className="quote--bg-pic">
+            <PatternQuotes />
+          </div>
+          <p className="quote--text">{data[currIdx].quote}</p>
+          <div className="author">
+            <p className="quote--author">{data[currIdx].author}</p>
+            <p className="quote--role">{data[currIdx].role}</p>
+          </div>
+        </div>
+        <div className="picture">
+          <div className="bg-picture">
+            <PatternBg />
+          </div>
+          <img src={require(`${data[currIdx].image}`)}></img>
+          <div className="button">
+            <button className="left-btn" onClick={clickPrev}>
+              <Prev />
+            </button>
+            <button className="right-btn" onClick={clickNext}>
+              <Next />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="curve">
+        <PatternCurve />
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const data = [
+  {
+    quote:
+      "I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.",
+    author: "Tanya Sinclair",
+    image: "./img/image-tanya.jpg",
+    role: "UX Engineer",
+  },
+  {
+    quote:
+      "If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.",
+    author: "John Tarkpor",
+    image: "./img/image-john.jpg",
+    role: "Junior Front-end Developer",
+  },
+];
